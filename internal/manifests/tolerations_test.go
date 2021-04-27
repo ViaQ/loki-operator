3cdd30cef38a6d896dad9bd8d066c45846be6a31
Brett Jones <blockloop@users.noreply.github.com>
2021-04-27 09:38:58 -0500

    add test for empty tolerations

--- internal/manifests/tolerations_test.go
+++ internal/manifests/tolerations_test.go
@@ -4,69 +4,62 @@ import (
 	"testing"
 
 	lokiv1beta1 "github.com/ViaQ/loki-operator/api/v1beta1"
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 )
 
 func TestTolerationsAreSetForEachComponent(t *testing.T) {
-	expected := []corev1.Toleration{{
+	tolerations := []corev1.Toleration{{
 		Key:      "type",
 		Operator: corev1.TolerationOpEqual,
 		Value:    "storage",
 		Effect:   corev1.TaintEffectNoSchedule,
 	}}
-	opts := Options{
-		Name:      uuid.New().String(),
-		Namespace: uuid.New().String(),
-		Image:     uuid.New().String(),
+	optsWithTolerations := Options{
 		Stack: lokiv1beta1.LokiStackSpec{
 			Template: lokiv1beta1.LokiTemplateSpec{
 				Compactor: lokiv1beta1.LokiComponentSpec{
-					Tolerations: expected,
+					Tolerations: tolerations,
 				},
 				Distributor: lokiv1beta1.LokiComponentSpec{
-					Tolerations: expected,
+					Tolerations: tolerations,
 				},
 				Ingester: lokiv1beta1.LokiComponentSpec{
-					Tolerations: expected,
+					Tolerations: tolerations,
 				},
 				Querier: lokiv1beta1.LokiComponentSpec{
-					Tolerations: expected,
+					Tolerations: tolerations,
 				},
 				QueryFrontend: lokiv1beta1.LokiComponentSpec{
-					Tolerations: expected,
+					Tolerations: tolerations,
 				},
 			},
 		},
 		ObjectStorage: ObjectStorage{},
 	}
 
-
 	t.Run("distributor", func(t *testing.T) {
-		d := NewDistributorDeployment(opts)
-		assert.Equal(t, expected, d.Spec.Template.Spec.Tolerations)
+		assert.Equal(t, tolerations, NewDistributorDeployment(optsWithTolerations).Spec.Template.Spec.Tolerations)
+		assert.Empty(t, NewDistributorDeployment(Options{}).Spec.Template.Spec.Tolerations)
 	})
 
-
 	t.Run("query_frontend", func(t *testing.T) {
-		q := NewQueryFrontendDeployment(opts)
-		assert.Equal(t, expected, q.Spec.Template.Spec.Tolerations)
+		assert.Equal(t, tolerations, NewQueryFrontendDeployment(optsWithTolerations).Spec.Template.Spec.Tolerations)
+		assert.Empty(t, NewQueryFrontendDeployment(Options{}).Spec.Template.Spec.Tolerations)
 	})
 
 	t.Run("querier", func(t *testing.T) {
-		q := NewQuerierStatefulSet(opts)
-		assert.Equal(t, expected, q.Spec.Template.Spec.Tolerations)
+		assert.Equal(t, tolerations, NewQuerierStatefulSet(optsWithTolerations).Spec.Template.Spec.Tolerations)
+		assert.Empty(t, NewQuerierStatefulSet(Options{}).Spec.Template.Spec.Tolerations)
 	})
 
 	t.Run("ingester", func(t *testing.T) {
-		i := NewIngesterStatefulSet(opts)
-		assert.Equal(t, expected, i.Spec.Template.Spec.Tolerations)
+		assert.Equal(t, tolerations, NewIngesterStatefulSet(optsWithTolerations).Spec.Template.Spec.Tolerations)
+		assert.Empty(t, NewIngesterStatefulSet(Options{}).Spec.Template.Spec.Tolerations)
 	})
 
 	t.Run("compactor", func(t *testing.T) {
-		c := NewCompactorStatefulSet(opts)
-		assert.Equal(t, expected, c.Spec.Template.Spec.Tolerations)
+		assert.Equal(t, tolerations, NewCompactorStatefulSet(optsWithTolerations).Spec.Template.Spec.Tolerations)
+		assert.Empty(t, NewCompactorStatefulSet(Options{}).Spec.Template.Spec.Tolerations)
 	})
 }
-
